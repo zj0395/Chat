941388ea3c8a92fbcd4a26298d01ddde4ab90214
zj0395 <zhangjian0395@163.com>
2018-12-04 20:04:11 +0800

    update singleton

--- common/singleton.h
+++ common/singleton.h
@@ -5,33 +5,24 @@
 #ifndef CHAT_SINGLETON_H
 #define CHAT_SINGLETON_H
 
-#include <memory>
-#include <iostream>
-
 namespace zj {
     template <typename T>
     class Singleton {
     public:
-        static T* getInstance() {
-            return m_instance;
+        static T& getInstance() {
+            return s_instance;
         }
 
     protected:
-        Singleton() { m_instance = instance.get(); };
+        Singleton() = default;
         ~Singleton() = default;
         Singleton(Singleton&) = delete;
         Singleton(Singleton&&) = delete;
-        static void destoryInstance( T *t) {
-            delete t;
-        }
-        static std::shared_ptr<T> instance;
-        static T *m_instance;
+        static T s_instance;
     };
 
     template <typename T>
-    std::shared_ptr<T> Singleton<T>::instance(new T, Singleton<T>::destoryInstance);
-    template <typename T>
-    T* Singleton<T>::m_instance = instance.get();
-}
+    T Singleton<T>::s_instance;
+};
 
 #endif //CHAT_SINGLETON_H

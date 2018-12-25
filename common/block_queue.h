084326a3eec2bd9df2e154f75e49dc0351a39a84
zhangjian <zhangjian0395@163.com>
2018-12-25 20:00:02 +0800

    add func, change style

--- common/block_queue.h
+++ common/block_queue.h
@@ -10,27 +10,35 @@
 #include <condition_variable>
 #include <queue>
 
+// size(), empty() 没有必要做到线程安全
 template <typename T>
 class BlockQueue {
 public:
+    std::size_t size() {
+        return _queue.size();
+    }
+    bool empty() {
+        return _queue.empty();
+    }
+
     // 线程安全的函数
     void push(const T& node) {
         std::lock_guard<std::mutex> guard(_mutex);
-        queues.push(node);
-        if (queues.size() == 1) {
+        _queue.push(node);
+        if (_queue.size() == 1) {
             _condition.notify_one();
         }
     }
     T pop() {
         std::unique_lock<std::mutex> guard(_mutex);
-        _condition.wait(guard, [&]{return !queues.empty();});
-        T data = queues.front();
-        queues.pop();
+        _condition.wait(guard, [&]{return !_queue.empty();});
+        T data = _queue.front();
+        _queue.pop();
         return data;
     }
     void clear() {
         std::lock_guard<std::mutex> guard(_mutex);
-        std::queue<T>().swap(queues);
+        std::queue<T>().swap(_queue);
     }
     // 放置一个空包，以退出阻塞中的pop
     void quit() {
@@ -38,7 +46,7 @@ public:
     }
 
 private:
-    std::queue<T> queues;
+    std::queue<T> _queue;
     std::mutex _mutex;
     std::condition_variable _condition;
 };
